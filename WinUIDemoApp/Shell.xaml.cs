680d3a5ec2089bc6f27197f764aa112c12345b9a
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-10-29 10:35:28 +0900

    Add demo code

--- WinUIDemoApp/Shell.xaml.cs
+++ WinUIDemoApp/Shell.xaml.cs
@@ -1,10 +1,31 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
 
 namespace WinUIDemoApp;
 
+public partial class Member : ObservableObject
+{
+    [ObservableProperty]
+    private int _id;
+
+    [ObservableProperty]
+    private string _name = string.Empty;
+}
+
 public partial class ShellViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private ObservableCollection<Member> _members = [];
+
+    public ShellViewModel()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Members.Add(new Member { Id = i, Name = $"Item {i}" });
+        }
+    }
 }
 
 public sealed partial class Shell : Page
@@ -15,4 +36,12 @@ public sealed partial class Shell : Page
     }
 
     public ShellViewModel ViewModel { get; } = new();
+
+    private void GoToMemberButton_Click(object sender, RoutedEventArgs e)
+    {
+        int memberIndex = (int)MemberIndexNumberBox.Value;
+        var memberItem = MemberItemsRepeater.GetOrCreateElement(memberIndex);
+        memberItem.UpdateLayout();
+        memberItem.StartBringIntoView();
+    }
 }

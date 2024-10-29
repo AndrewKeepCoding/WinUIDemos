19109e716c012af2b999863b2812b8165c1c9d16
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-10-29 09:59:26 +0900

    Add ShellViewModel

--- WinUIDemoApp/Shell.xaml.cs
+++ WinUIDemoApp/Shell.xaml.cs
@@ -1,10 +1,18 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml.Controls;
 
 namespace WinUIDemoApp;
+
+public partial class ShellViewModel : ObservableObject
+{
+}
+
 public sealed partial class Shell : Page
 {
     public Shell()
     {
         InitializeComponent();
     }
+
+    public ShellViewModel ViewModel { get; } = new();
 }

b2cf88ff7446d15d32530833491a8ef88051db26
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-10-30 11:58:39 +0900

    Add demo code

--- WinUIDemoApp/MainWindow.xaml.cs
+++ WinUIDemoApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using Microsoft.UI.Xaml;
 
 namespace WinUIDemoApp;
+
 public sealed partial class MainWindow : Window
 {
     public MainWindow()
--- WinUIDemoApp/Shell.xaml.cs
+++ WinUIDemoApp/Shell.xaml.cs
@@ -1,5 +1,11 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace WinUIDemoApp;
 
@@ -9,10 +15,32 @@ public partial class ShellViewModel : ObservableObject
 
 public sealed partial class Shell : Page
 {
+    private ObservableCollection<string> Items = [];
+
     public Shell()
     {
         InitializeComponent();
     }
 
     public ShellViewModel ViewModel { get; } = new();
+
+    private void Grid_DragOver(object sender, DragEventArgs e)
+    {
+        e.AcceptedOperation = DataPackageOperation.Copy;
+    }
+
+    private async void Grid_Drop(object sender, DragEventArgs e)
+    {
+        Items.Clear();
+
+        if (e.DataView.Contains(StandardDataFormats.StorageItems) is false)
+        {
+            return;
+        }
+
+        foreach (var item in await e.DataView.GetStorageItemsAsync())
+        {
+            Items.Add(item.Path);
+        }
+    }
 }

94eab9ea2f67ec56d899bb792b9d95657011acce
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-11-19 17:43:03 +0900

    Add demo code

---
+++ WinUIDemoApp/Controls/AwesomeCustomControl.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace WinUIDemoApp.Controls;
+
+public class AwesomeCustomControl : Control
+{
+    public AwesomeCustomControl()
+    {
+        DefaultStyleKey = typeof(AwesomeCustomControl);
+    }
+}

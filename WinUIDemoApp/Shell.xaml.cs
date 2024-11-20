30d8cb72798e71a8b4abe8c041a7426b59508cc8
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-11-20 11:29:17 +0900

    Add demo code

--- WinUIDemoApp/Shell.xaml.cs
+++ WinUIDemoApp/Shell.xaml.cs
@@ -1,4 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using Microsoft.UI;
 using Microsoft.UI.Xaml.Controls;
 
 namespace WinUIDemoApp;
@@ -12,7 +14,23 @@ public sealed partial class Shell : Page
     public Shell()
     {
         InitializeComponent();
+        Unloaded += (sender, e) =>
+        {
+            CanvasControl.RemoveFromVisualTree();
+            CanvasControl = null;
+        };
     }
 
     public ShellViewModel ViewModel { get; } = new();
+
+    private void CanvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+    {
+        var drawingSession = args.DrawingSession;
+        drawingSession.DrawLine(
+            x0: 0,
+            y0: 0,
+            x1: (float)sender.ActualWidth,
+            y1: (float)sender.ActualHeight,
+            Colors.SkyBlue);
+    }
 }

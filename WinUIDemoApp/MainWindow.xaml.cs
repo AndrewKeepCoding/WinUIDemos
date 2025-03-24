1b59549224e60f56dea19e0b47209eaf76e46dd9
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2025-03-24 10:01:08 +0900

    Add sample code

--- WinUIDemoApp/MainWindow.xaml.cs
+++ WinUIDemoApp/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
 using Microsoft.UI.Xaml;
 
 namespace WinUIDemoApp;
+
 public sealed partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        SetTitleBar(TitleBarControl);
+        ExtendsContentIntoTitleBar = true;
+        AppWindow.SetIcon("Assets/GalleryIcon.ico");
     }
 }

032be2652174d42e2fb2cc3ad723f616e2aea3d5
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2025-03-29 22:32:30 +0900

    Add sample code

--- WinUIDemoApp/App.xaml.cs
+++ WinUIDemoApp/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.UI.Xaml;
+using Serilog;
+using System;
 
 namespace WinUIDemoApp;
 
@@ -14,15 +17,34 @@ public partial class App : Application
 
     private Window? Window { get; set; }
 
+    public static T GetService<T>() where T : class
+    {
+        if ((App.Current as App)?.Host?.Services.GetService(typeof(T)) is T service)
+        {
+            return service;
+        }
+
+        throw new InvalidOperationException($"Service of type {typeof(T).Name} not found.");
+    }
+
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
         Host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
             .ConfigureServices((context, services) =>
             {
+                services.AddSingleton<ShellViewModel>();
             })
+            .UseSerilog((context, services, configuration) => configuration
+                //.WriteTo.Console()
+                //.WriteTo.File(
+                //    path: "D:\\Projects\\AndrewKeepCoding\\WinUIDemos\\WinUIDemoApp\\.log",
+                //    rollingInterval: RollingInterval.Day)
+                .WriteTo.Debug())
             .Build();
 
         Window = new MainWindow();
         Window.Activate();
+
+        Serilog.Log.Information("App.OnLaunched");
     }
 }
--- WinUIDemoApp/Shell.xaml.cs
+++ WinUIDemoApp/Shell.xaml.cs
@@ -1,10 +1,24 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace WinUIDemoApp;
 
-public partial class ShellViewModel : ObservableObject
+public partial class ShellViewModel(ILogger<ShellViewModel> logger) : ObservableObject
 {
+    private readonly ILogger<ShellViewModel> _logger = logger;
+
+    [ObservableProperty]
+    public partial int Count { get; set; }
+
+    [RelayCommand]
+    private void Increment()
+    {
+        Count++;
+        _logger.LogInformation("Incremented count to {Count}.", Count);
+    }
 }
 
 public sealed partial class Shell : Page
@@ -12,7 +26,13 @@ public sealed partial class Shell : Page
     public Shell()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
     }
 
-    public ShellViewModel ViewModel { get; } = new();
+    public ShellViewModel ViewModel { get; } = App.GetService<ShellViewModel>();
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        Serilog.Log.Information("Shell.OnLoaded");
+    }
 }

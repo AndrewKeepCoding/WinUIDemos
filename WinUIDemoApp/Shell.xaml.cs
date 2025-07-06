85ec4dd2c979a5050f8ddc470032639d2ec613c5
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2025-07-06 15:54:07 +0900

    Add demo code

--- WinUIDemoApp/Shell.xaml.cs
+++ WinUIDemoApp/Shell.xaml.cs
@@ -1,10 +1,48 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace WinUIDemoApp;
 
-public partial class ShellViewModel : ObservableObject
+public static class Helpers
 {
+    public static string GetErrorMessage(string propertyName, IEnumerable<ValidationResult> validationResults) =>
+        validationResults.FirstOrDefault(result => result.MemberNames.Contains(propertyName))?.ErrorMessage ?? string.Empty;
+}
+
+public partial class ShellViewModel : ObservableValidator
+{
+    [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [MinLength(1, ErrorMessage = "Name must be at least 1 character long.")]
+    public partial string Name { get; set; } = string.Empty;
+
+
+    [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [Range(0, 200)]
+    public partial int Age { get; set; }
+
+    public ShellViewModel()
+    {
+        ErrorsChanged += ShellViewModel_ErrorsChanged;
+    }
+
+    public ObservableCollection<ValidationResult> ValidationResults { get; private set; } = [];
+
+    private void ShellViewModel_ErrorsChanged(object? sender, DataErrorsChangedEventArgs e)
+    {
+        ValidationResults.Clear();
+
+        foreach (ValidationResult validationResult in GetErrors())
+        {
+            ValidationResults.Add(validationResult);
+        }
+    }
 }
 
 public sealed partial class Shell : Page

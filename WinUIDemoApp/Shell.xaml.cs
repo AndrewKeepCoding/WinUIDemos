4cd68d1dd0e3f3b8204c679ebc4fa9ae5455283f
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2025-10-28 14:39:20 +0900

    Add demo code

--- WinUIDemoApp/Shell.xaml.cs
+++ WinUIDemoApp/Shell.xaml.cs
@@ -1,4 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.WinUI.Controls;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace WinUIDemoApp;
@@ -15,4 +17,40 @@ public sealed partial class Shell : Page
     }
 
     public ShellViewModel ViewModel { get; } = new();
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        var element = new Button()
+        {
+            Content = $"{this.WrapPanelControl.Children.Count + 1}",
+            HorizontalAlignment = HorizontalAlignment.Stretch,
+            VerticalAlignment = VerticalAlignment.Stretch
+        };
+
+        this.WrapPanelControl.Children.Add(element);
+    }
+
+    private void RemoveButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (this.WrapPanelControl.Children.Count == 0)
+        {
+            return;
+        }
+
+        this.WrapPanelControl.Children.RemoveAt(this.WrapPanelControl.Children.Count - 1);
+    }
+
+    private void OrientationButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.WrapPanelControl.Orientation = this.WrapPanelControl.Orientation == Orientation.Horizontal
+            ? Orientation.Vertical
+            : Orientation.Horizontal;
+    }
+
+    private void StretchChildButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.WrapPanelControl.StretchChild = this.WrapPanelControl.StretchChild == StretchChild.None
+            ? StretchChild.Last
+            : StretchChild.None;
+    }
 }

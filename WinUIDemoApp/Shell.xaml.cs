83fdbbe1de9ebb92a11ccd7f47a98daa6d87f0fe
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2025-12-22 10:48:51 +0900

    Add demo code

--- WinUIDemoApp/Shell.xaml.cs
+++ WinUIDemoApp/Shell.xaml.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace WinUIDemoApp;
@@ -12,7 +13,31 @@ public sealed partial class Shell : Page
     public Shell()
     {
         InitializeComponent();
+        RequestedTheme = ElementTheme.Light;
     }
 
     public ShellViewModel ViewModel { get; } = new();
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+
+        var dialog = new ContentDialog
+        {
+            Title = "Hello",
+            XamlRoot = this.XamlRoot,
+            Content = "This is a ContentDialog in WinUI 3!",
+            CloseButtonText = "OK",
+        };
+
+        var parent = (sender as FrameworkElement)?.Parent as FrameworkElement;
+        var requestedTheme = parent?.RequestedTheme;
+        var actualTheme = parent?.ActualTheme;
+
+        if ((sender as FrameworkElement)?.Parent is FrameworkElement { ActualTheme: ElementTheme theme })
+        {
+            dialog.RequestedTheme = theme;
+        }
+
+        _ = dialog.ShowAsync();
+    }
 }
